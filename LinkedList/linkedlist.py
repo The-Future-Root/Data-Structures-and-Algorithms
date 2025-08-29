aae6bf0f0721e55e1381208758c30621d7132edd
Ashwin V Kumar <122103046+github-ashwin@users.noreply.github.com>
2025-08-29 20:27:19 +0530

    Adding Doubly LinkedList

--- LinkedList/linkedlist.py
+++ LinkedList/linkedlist.py
@@ -138,4 +138,157 @@ if __name__ == "__main__":
 
 # Doubly Linked-list
 
+class DLLNode:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def is_empty(self):
+        return self.head is None
+
+    def traverse_forward(self):
+        if self.is_empty():
+            print("Empty List")
+            return
+        temp = self.head
+        while temp:
+            print(temp.value, end=" <-> ")
+            temp = temp.next
+        print("None")
+
+    def traverse_backward(self):
+        if self.is_empty():
+            print("Empty List")
+            return
+        temp = self.tail
+        while temp:
+            print(temp.value, end=" <-> ")
+            temp = temp.prev
+        print("None")
+
+    def length(self):
+        count = 0
+        temp = self.head
+        while temp:
+            count += 1
+            temp = temp.next
+        return count
+
+    def insert_begin(self, value):
+        new_node = DLLNode(value)
+        if self.is_empty():
+            self.head = self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+
+    def insert_end(self, value):
+        new_node = DLLNode(value)
+        if self.is_empty():
+            self.head = self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+
+    def insert_pos(self, pos, value):
+        if pos < 0 or pos > self.length():
+            print("Invalid Position")
+            return
+        
+        if pos == 0:
+            self.insert_begin(value)
+            return
+        
+        if pos == self.length():
+            self.insert_end(value)
+            return
+        
+        new_node = DLLNode(value)
+        temp = self.head
+        for _ in range(pos - 1):
+            temp = temp.next
+        new_node.next = temp.next
+        new_node.prev = temp
+        temp.next.prev = new_node
+        temp.next = new_node
+
+    def delete_begin(self):
+        if self.is_empty():
+            print("Empty List")
+            return
+        
+        if self.head == self.tail:
+            self.head = self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+
+    def delete_end(self):
+        if self.is_empty():
+            print("Empty List")
+            return
+        
+        if self.head == self.tail:
+            self.head = self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+
+    def delete_val(self, value):
+        if self.is_empty():
+            print("Empty List")
+            return
+        
+        temp = self.head
+        while temp:
+            if temp.value == value:
+
+                if temp == self.head:
+                    self.delete_begin()
+
+                elif temp == self.tail:
+                    self.delete_end()
+
+                else:
+                    temp.prev.next = temp.next
+                    temp.next.prev = temp.prev
+                return
+            temp = temp.next
+        print(f"{value} not found in the list")
+
+    def search(self, value):
+        temp = self.head
+        while temp:
+            if temp.value == value:
+                print(f"{value} is Present")
+                return True
+            temp = temp.next
+        print(f"{value} is Not Present")
+        return False
+
+
+if __name__ == "__main__":
+    dll = DoublyLinkedList()
+    dll.insert_end(10)
+    dll.insert_end(20)
+    dll.insert_begin(5)
+    dll.insert_pos(1, 15)
+    dll.traverse_forward()
+    dll.traverse_backward()
+    print("Length:", dll.length())
+    dll.delete_val(10)
+    dll.traverse_forward()
+    dll.delete_begin()
+    dll.traverse_forward()
+    dll.delete_end()
+    dll.traverse_forward()
 
